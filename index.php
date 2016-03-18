ea6fd618c65e0d4dade3735bc14219714cfb8865
MattCouv <matth.couvreur@gmail.com>
2016-03-18 13:39:21 +0100

    Upload test

--- index.php
+++ index.php
@@ -1,31 +1,16 @@
 <?php
-
-include dirname(__DIR__) . '/upload.class/UploadFile.php';
+include dirname(__DIR__) . '/UploadImagesPhp/UploadFile.php';
 
 $image = new UploadFile();
 
-if (!empty($_FILES['image1'])) {
-  $file = $_FILES['image1'];
-  if($image->upload($file)){
-    $image->resize($file);
-    echo $image->getId();
-  }
+if (!empty($_FILES)) {
+  $file = $_FILES['foo'];
+  $image->upload($file);
+  $image->resize($file);
 }
 
  ?>
-
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>test</title>
-  </head>
-  <body>
     <form method="post" enctype="multipart/form-data">
-      <input type="file" name="image1">
-	 	   <input type="submit" name="upload_image" value="Upload">
-
+      <input type="file" name="foo">
+	 	  <input type="submit" value="Upload">
     </form>
-  </body>
-</html>
